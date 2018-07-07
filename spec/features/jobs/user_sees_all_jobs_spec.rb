0b9cff160576772c0bf9c32b5d622253880a9b8a
brickstar <brickstar@gmail.com>
2018-07-07 13:59:50 -0600

    added and tested links to create company, job on company index and job index - all tests pass

--- spec/features/jobs/user_sees_all_jobs_spec.rb
+++ spec/features/jobs/user_sees_all_jobs_spec.rb
@@ -7,10 +7,21 @@ describe "User sees all jobs" do
     job_1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category: category)
     job_2 = company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City", category: category)
 
-    visit company_path(company)
+    visit company_jobs_path(company)
 
     expect(page).to have_content(company.name)
     expect(page).to have_content(job_1.title)
     expect(page).to have_content(job_2.title)
   end
+
+  it 'can link to job new' do
+    company = Company.create!(name: "ESPN")
+
+    visit company_jobs_path(company)
+
+    click_link 'Create a New Job'
+
+    expect(current_path).to eq(new_company_job_path(company))
+  end
+
 end
